0e63ea78c9dcc48e174e0ecab5284557419edaad
Surprise Mnisi <surprise@cubezoo.joburg>
2023-05-22 16:48:22 +0200

    Init

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/comments', async (req, res) => {
+  try {
+    const response = await axios.get('https://comments-api.herokuapp.com/api/comments'); // Replace with your API endpoint URL
+
+    const comments = response.data.comments;
+    res.json(comments);
+  } catch (error) {
+    console.error('Error retrieving comments:', error.message);
+    res.status(500).json({ error: 'Failed to retrieve comments' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
