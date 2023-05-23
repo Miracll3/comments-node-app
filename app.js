664ef1a11ec6b7d3a62017a4bb1fc19ecf0cb5fa
Surprise Mnisi <surprise@cubezoo.joburg>
2023-05-23 07:09:09 +0200

    base url

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.get('/comments', async (req, res) => {
   try {
-    const response = await axios.get('https://comments-api.herokuapp.com/api/comments'); // Replace with your API endpoint URL
+    const response = await axios.get('https://comments-api.herokuapp.com/api/comments'); // API endpoint URL
 
     const comments = response.data.comments;
     res.json(comments);
@@ -14,6 +15,14 @@ app.get('/comments', async (req, res) => {
     console.error('Error retrieving comments:', error.message);
     res.status(500).json({ error: 'Failed to retrieve comments' });
   }
+});
+
+app.get('/', (req, res) => {
+  // Use the path module to get the absolute path to the HTML file
+  const filePath = path.join(__dirname, 'index.html');
+  
+  // Send the HTML file as the response
+  res.sendFile(filePath);
 });
 
 app.listen(PORT, () => {
